d037b762703c1e3740d6dba206e815754b514a9b
Thiago Goncalves <thimo_28@hotmail.com>
2023-01-03 17:24:32 -0800

    Add event test

---
+++ repository-project/src/domain/event/@shared/event-dispatcher.spec.ts
@@ -0,0 +1,12 @@
+describe("Domain Event tests", () => {
+
+    it("should register an event handler", () => {
+
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(1);
+    });
+});
